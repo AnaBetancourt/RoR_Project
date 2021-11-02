666061ad2c791784833af37a6fb86de748701961
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-11-02 00:40:34 -0400

    removed extra CRUD functionality features

--- app/controllers/data_logs_controller.rb
+++ app/controllers/data_logs_controller.rb
@@ -11,15 +11,6 @@ class DataLogsController < ApplicationController
     def create
     end
 
-    def show
-    end
-
-    def edit
-    end
-
-    def update
-    end
-
     def destroy
     end
 
