55774bf2d2eea3fb9d9523bc5efae898b5b58f98
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-11-02 00:37:40 -0400

    began setting up controller

--- app/controllers/data_logs_controller.rb
+++ app/controllers/data_logs_controller.rb
@@ -1,2 +1,26 @@
 class DataLogsController < ApplicationController
+    
+    def index
+        @data_logs = DataLog.all
+    end
+
+    def new
+        @data_log = DataLog.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 end
