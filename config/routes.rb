cbdb9b90c507a3670334a51ddd3fabfe838ee2b1
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-11-04 16:04:00 -0400

    modified routes to only allowed used routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'application#landing'
-  resources :data_logs
+  resources :data_logs, except: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
