df969b620cdef128de8f3b87fee36aa9c870c8f9
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-11-04 15:17:48 -0400

    grabbed destination IP address from log to display

--- app/models/data_log.rb
+++ app/models/data_log.rb
@@ -1,2 +1,14 @@
 class DataLog < ApplicationRecord
+
+    def source_ip
+
+    end
+
+    def destination_ip
+        starting_index = self.log.index("dst")
+        ending_index = starting_index + 15
+        substring = self.log[starting_index...ending_index]
+
+        return substring[4...ending_index]
+    end
 end
