56bc01675a967b7f3fccc5c2140dbc177c7528ee
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-11-04 15:23:08 -0400

    grabbed source address from log to display

--- app/models/data_log.rb
+++ app/models/data_log.rb
@@ -1,7 +1,11 @@
 class DataLog < ApplicationRecord
 
     def source_ip
+        starting_index = self.log.index("sourceTranslatedAddress")
+        ending_index = starting_index + 37
+        substring = self.log[starting_index...ending_index]
 
+        return substring[24...ending_index]
     end
 
     def destination_ip
